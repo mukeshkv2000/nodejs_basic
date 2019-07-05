153e919b4b33aa817e0cbc8571bb7082f98d31e3
mukesh <mkvaidya2000@gmail.com>
2019-07-06 00:06:00 +0530

    updated

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
-http.createServer( (req, res) => {
-
-
- 
+http.createServer( (req, res) => { 
 
     console.log(req.url, req.method, req.headers);
-    process.exit();
-
+   // process.exit();
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head> <title> Node basic server page </title></head> ');
+    res.write('<body><h1> Hello From backend Node js server </h1>  </body>');
+    res.write('</html>');
+res.end();
 
 }).listen(3000);
